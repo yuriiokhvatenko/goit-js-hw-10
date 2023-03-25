c6692c68f5673ee0d4c389aee2563e55671d3009
Yurii Okhvatenko <fess517@gmail.com>
2023-03-25 14:28:33 +0200

    replace const

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
 export default { fetchCountries }
 
-const URL = `https://restcountries.com/v3.1/name`;
-const FILTER = `name,capital,population,flags,languages`;
-
 function fetchCountries(country) {
 
+  const URL = `https://restcountries.com/v3.1/name`;
+  const FILTER = `name,capital,population,flags,languages`;
+  
   return fetch(`${URL}/${country}?fields=${FILTER}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
